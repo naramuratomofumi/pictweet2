3278fb5834946615a374a5954007979e9aa4baf2
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-27 18:34:02 +0900

    ツイート更新処理の実装(updateアクションのルーティングを設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'tweets#index' #http://localhost:3000へアクセスすると、Railsの「初期画面」が表示されてしまいます。このパスにアクセスしても、トップページを表示させるようにルーティングを編集
-  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update]
 end
