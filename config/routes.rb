ebf803f93b9bb44b3f4cd8433d5ccc0bba1f18b2
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-27 20:14:06 +0900

    ターミナルにてrails g devise userの実行(deviseのUserモデルを作成)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'tweets#index' #http://localhost:3000へアクセスすると、Railsの「初期画面」が表示されてしまいます。このパスにアクセスしても、トップページを表示させるようにルーティングを編集
   resources :tweets
 end
