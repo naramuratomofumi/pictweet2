1c667ca666aaaa01da847e706b89def1c49a4782
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-29 16:04:05 +0900

    コメント機能の実装(ネストを用いたルーティングの設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index' #http://localhost:3000へアクセスすると、Railsの「初期画面」が表示されてしまいます。このパスにアクセスしても、トップページを表示させるようにルーティングを編集
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: :create #ルーティングのネスト どのツイートに対するコメントなのかをパスから判断できるようにする
+  end
   resources :users, only: :show
 end
