5af47e5e8409663ec880d825e2ded51f2165b145
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-29 17:45:51 +0900

    検索機能の実装(collectionを使ってアクションを全体に指定する。)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'tweets#index' #http://localhost:3000へアクセスすると、Railsの「初期画面」が表示されてしまいます。このパスにアクセスしても、トップページを表示させるようにルーティングを編集
   resources :tweets do
     resources :comments, only: :create #ルーティングのネスト どのツイートに対するコメントなのかをパスから判断できるようにする
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
