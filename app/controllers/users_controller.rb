e20bcc9fcae72a30d0c5a88e6b24b901b09b6079
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-29 14:18:16 +0900

    投稿者のマイページにアクセスできるようにする(クリックされたユーザーのidから情報を取得して、ビューに受け渡す)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets
+    user = User.find(params[:id]) #ツイートの右下に表示されるユーザー名をクリックすることで、送られたidをparamsで取得し、そのユーザーのレコードを変数userに代入しています。この変数を利用して、各インスタンス変数ではcurrent_userを変数userに変えました
+    @nickname = user.nickname     #@nicknameでは、ユーザーのnicknameを取り出しています。
+    @tweets = user.tweets         #アソシエーションを利用しuser.tweetsとすることで、そのユーザーが投稿したツイートを取得して、@tweetsに代入しています
   end
 end
