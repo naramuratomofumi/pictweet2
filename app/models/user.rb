a814a0837f044e168dd082acf7bc4789f00b842b
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-29 11:32:20 +0900

    モデル同士を関連付けよう(has_manyを使ってuserは複数の投稿を所有するというアソシエーションを定義)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :tweets #userは複数の投稿を所有する
 end
